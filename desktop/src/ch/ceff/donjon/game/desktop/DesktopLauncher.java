cc1f7b84936f29675b416a43b8ead45340f9edf4
CP-12GSP <ga.sprunger@gmail.com>
2015-06-12 08:55:10 +0200

    First Commit of the Project

---
+++ desktop/src/ch/ceff/donjon/game/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package ch.ceff.donjon.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import ch.ceff.donjon.game.Donjon;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new Donjon(), config);
+    }
+}
